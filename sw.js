42131ffcb89a9d7160e771df23a8f806d89b0208
tobiasgoijman-1620 <tobias.goijman@gmail.com>
2026-05-05 23:34:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,8 +1,7 @@
-// Service worker minimo - no cachea el HTML para que siempre tome la version nueva
+const VERSION = 'v' + Date.now();
 self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
   self.clients.claim();
 });
-// No cachea nada - siempre va a la red
 self.addEventListener('fetch', e => {});
