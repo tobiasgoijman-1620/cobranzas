f9be34cd8995066123c9105156cd129bafcfb484
tobiasgoijman-1620 <tobias.goijman@gmail.com>
2026-05-05 23:21:21 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,36 +1,8 @@
-const CACHE = 'cobranzas-v1';
-const ASSETS = [
-  '/index.html',
-  '/manifest.json'
-];
-
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
-  );
-});
-
+// Service worker minimo - no cachea el HTML para que siempre tome la version nueva
+self.addEventListener('install', e => self.skipWaiting());
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    ).then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+  self.clients.claim();
 });
-
-self.addEventListener('fetch', e => {
-  // Solo cachear GET, no las llamadas a APIs externas
-  if (e.request.method !== 'GET') return;
-  const url = new URL(e.request.url);
-  if (url.hostname.includes('supabase') || url.hostname.includes('anthropic') || url.hostname.includes('googleapis')) return;
-
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
-      })
-      .catch(() => caches.match(e.request))
-  );
-});
+// No cachea nada - siempre va a la red
+self.addEventListener('fetch', e => {});
